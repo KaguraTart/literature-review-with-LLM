bce50078ee2948b94b2a87e2e6b52fd74d85bc03
kaguratart <kaguratart@gmail.com>
2026-06-14 22:58:33 +0800

    Release v0.1.1

--- addon/content/workbench.js
+++ addon/content/workbench.js
@@ -2129,7 +2129,7 @@ function localAgentConnectionTestRequestForProfile(profile) {
         capabilities: {},
         clientInfo: {
           name: "literature-review-with-llm-workbench",
-          version: "0.1.0"
+          version: "0.1.1"
         }
       }
     }
